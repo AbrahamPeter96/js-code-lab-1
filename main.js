4a0d8ecfeffcaded3efc62b3b3e6994a1c3adb6b
mudaseriqbalshah <mudaseriqbalshah@gmail.com>
2021-08-22 15:01:09 +0500

    in between

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 import {
   getPresalesApproved,
   getPresalesNotApproved,
-  getPresalesNotApprovedAddresses
+  getPresalesNotApprovedAddresses,
 } from './multicall.js';
-
+import BigNumber from 'bignumber.js';
+import Web3 from 'web3';
 const init = async () => {
   const presalesApproved = await getPresalesApproved();
   console.log('presalesApproved: ', presalesApproved);
 
   const presalesNotApproved = await getPresalesNotApproved();
-  console.log('presalesNotApproved: ', presalesNotApproved);
+  console.log('presalesNotApproved', presalesNotApproved);
+  console.log(
+    'presalesNotApproved: ',
 
+    typeof parseInt(presalesNotApproved[0].tokenXSupply.hex.toString())
+  );
   // const res = await getPresalesNotApprovedAddresses();
   // console.log('res: ', res);
 };
